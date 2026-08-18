3c838fa6f3bcba7decec65687112a0ee0d40799a
Amábili <amabili@gmail.com>
2026-08-17 21:54:39 -0300

    Aula03

--- MyApplication2/app/src/main/java/com/ifsc/app/MainActivity.java
+++ MyApplication2/app/src/main/java/com/ifsc/app/MainActivity.java
@@ -3,7 +3,9 @@ package com.ifsc.app;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +13,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
-    Integer contador = 0;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +29,38 @@ public class MainActivity extends AppCompatActivity {
         tv.setText("-");
 
         Button button=findViewById(R.id.button);
-        button.setText("Iniciar contagem");
+        button.setText("Gerar Número");
+
+        EditText edmin,edmax;
+        edmin=findViewById(R.id.edMin);
+        edmax=findViewById(R.id.edMax);
+
         button.setOnClickListener(v -> {
-            contador++;
-            tv.setText(Integer.toString(contador));
-            button.setText("Contar");
+
+            String smin=edmin.getText().toString();
+            String smax=edmax.getText().toString();
+
+            if(smin.isEmpty()){
+                edmin.setError("Informe um inteiro");
+                return;
+            }
+            if(smax.isEmpty()){
+                edmax.setError("Informe um número");
+            }
+
+            int min = Integer.parseInt(smin);
+            int max = Integer.parseInt(smax);
+
+            if(min>max){
+                Toast.makeText(this, "Defina mínimo menor que máximo", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Random random = new Random();
+            int randonN = random.nextInt(min,max);
+            tv.setText(Integer.toString(randonN));
+
+
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
