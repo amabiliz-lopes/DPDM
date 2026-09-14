0f52838e6f045bce0c5481d91d9f9cf3e8e30c18
Amabili <amabili@gmail.com>
2026-09-13 23:28:49 -0300

    Atividade EAD

--- MyApplication2/app/src/main/java/com/ifsc/app/MainActivity.java
+++ MyApplication2/app/src/main/java/com/ifsc/app/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ifsc.app;
 
+import android.content.ActivityNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,10 +11,16 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Random;
 import android.content.Intent;
 
@@ -36,5 +44,52 @@ public class MainActivity extends AppCompatActivity {
             startActivity(exemploExplicita);
         });
 
+        //Intent Implícita
+        buttonImplicita.setOnClickListener(v->{
+
+            try{
+                //Copia o PDF de res/raw para o cahce do app
+                File pastaCache = new File(getCacheDir(), "pdfs");
+                if(!pastaCache.exists()){
+                    pastaCache.mkdirs();
+                }
+                File arquivoPdf = new File(pastaCache, "intent_implicita.pdf");
+
+                if (!arquivoPdf.exists()) {
+                    InputStream input = getResources().openRawResource(R.raw.intent_implicita);
+                    OutputStream output = new FileOutputStream(arquivoPdf);
+
+                    byte[] buffer = new byte[1024];
+                    int tamanhoLido;
+                    while ((tamanhoLido = input.read(buffer)) != -1) {
+                        output.write(buffer, 0, tamanhoLido);
+                    }
+                    output.flush();
+                    output.close();
+                    input.close();
+                }
+
+                //Gera uma URI segura via FileProvider
+                Uri uri = FileProvider.getUriForFile(
+                        this,
+                        getPackageName() + ".provider",
+                        arquivoPdf
+                );
+
+                //Cria a Intent Implícita
+                Intent abrirPdf = new Intent(Intent.ACTION_VIEW);
+                abrirPdf.setDataAndType(uri, "application/pdf");
+                abrirPdf.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                //Apresenta o seletor de apps
+                Intent chooser = Intent.createChooser(abrirPdf, "Abrir PDF com...");
+                startActivity(chooser);
+
+            } catch (IOException e) {
+                Toast.makeText(this, "Erro ao ler o PDF.", Toast.LENGTH_SHORT).show();
+            } catch (ActivityNotFoundException e){
+                Toast.makeText(this, "Nenhum app encontrado para abrir o PDF.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
